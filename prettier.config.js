e26267b1a5203b94f16658ffabd8eabe0d9d7aa6
Michał Zegan <webczat@outlook.com>
2025-12-15 20:39:38 +0000

    Configure prettier formatter

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+ /**
+  * @type {import("prettier").Config}
+  */
+const config = {
+    arrowParens: "avoid",
+    objectWrap: "preserve",
+    quoteProps: "as-needed",
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 120,
+    semi: true,
+};
+
+export default config;
